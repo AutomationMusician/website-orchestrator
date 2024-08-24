0d5c3d20f188bb667bd3786cdc22a777f76429a9
Joshua Fishman <josh.a.fishman@gmail.com>
2024-08-24 10:50:54 -0400

    remove TXT record, duckdns doesn't send the ip address in the response

--- duckdns-update.sh
+++ duckdns-update.sh
@@ -6,17 +6,14 @@
 #    55 * * * * <absolute-path-to>/duckdns-update.sh
 
 SCRIPT_DIR=$(realpath $(dirname $0))
-echo $SCRIPT_DIR
-
 source ${SCRIPT_DIR}/.env
 LOGS_DIR=${SCRIPT_DIR}/logs/duckdns-update
 OUTPUT_FILE=${LOGS_DIR}/$(date '+%Y-%m-%d').txt
-echo $OUTPUT_FILE
 
 mkdir -p ${LOGS_DIR}
 touch $OUTPUT_FILE
 {
     date '+%H:%M:%S' && \
-    curl "https://www.duckdns.org/update?domains=${DUCKDNS_DOMAINS}&txt=${DUCKDNS_TXT_RECORD}&token=${DUCKDNS_TOKEN}&verbose=true"
+    curl "https://www.duckdns.org/update?domains=${DUCKDNS_DOMAINS}&token=${DUCKDNS_TOKEN}&verbose=true"
 } | tr '\n' ',' | tee -a $OUTPUT_FILE
 echo "" | tee -a $OUTPUT_FILE
